287f79e82390cabbb35ad346242243cb52f79d98
Леонид Кухарчик <kuxarchik007@mail.ru>
2024-09-02 20:42:42 +0300

    Убраны лишние методы

--- Chat.cpp
+++ Chat.cpp
@@ -16,39 +16,6 @@ void Chat::Users()
 	}
 }
 
-void Chat::Read_message(string& filename)
-{
-	/*while (running)
-	{
-		ifstream infile(filename);
-		if (infile.is_open())
-		{
-			string massege;
-			while (getline(infile, massege))
-			{
-				cout << "������� ���������: " << massege << endl;
-			}
-			infile.close();
-		}
-		std::this_thread::sleep_for(std::chrono::seconds(1));
-	}*/
-}
-
-void Chat::Send_message(std::string& filename)
-{
-	/*while (running)
-	{
-		string massage;
-		getline(cin, massage);
-		ofstream outfile(filename, ios_base::app);
-		if (outfile.is_open())
-		{
-			outfile << massage << endl;
-			outfile.close();
-		}
-	}*/
-}
-
 void Chat::Print_All_message()
 {
 	vector<string> messages;
--- Chat.h
+++ Chat.h
@@ -14,8 +14,6 @@ class Chat
 public:
 	bool running;
 	void Users();
-	void Read_message(string& filename);
-	void Send_message(string& filename);
 	void Print_All_message();
 	void Send_All_message();
 	void User_Choice();
